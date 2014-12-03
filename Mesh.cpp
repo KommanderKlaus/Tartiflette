811a8f2a2287a6afb020557de6cac4087c736e25
maxime <lordelbert@gmail.com>
2014-12-03 21:40:01 +0100

    Add init to vertexbuf

--- Mesh.cpp
+++ Mesh.cpp
@@ -38,10 +38,12 @@ int Mesh::load(const std::string& filename){
 			if(line[0] == 'v'){
 				// v 0.5 4.7 8.5
 				sscanf(line.data(), "%s %f %f %f", head.data(), &x, &y, &z);
+				std::cout << head.data() << std::endl;
 				vertexBuf.emplace(vertexBuf.end(), x, y, z);
 			}else if(line[0] == 'f'){
 				// f 1 5 4
 				sscanf(line.data(), "%s %d %d %d", head.data(), &s1, &s2, &s3);
+				vertexBuf.emplace(vertexBuf.end(), s1, s2, s3);
 				
 				// Génération d'un triangle
 				Triangle tri;
@@ -61,27 +63,30 @@ int Mesh::load(const std::string& filename){
 //				tri.e3->connectTo2(tri.e3);
 				m_tri.push_back(tri);
 				++curTriIndex;
+				
 				// Lier les triangles entre eux
 				for(int i = 0 ; i<curTriIndex; ++i){
-					Triangle * curTri = m_tri.data() + curTriIndex;
-					Triangle * oldTri = m_tri.data() + i;
+					Triangle  curTri = m_tri[curTriIndex];
+					Triangle  oldTri = m_tri[i];
 					// test de correspondance des sommets
-					if(curTri->e1->m_vertex == oldTri->e1->m_vertex){
-						if(curTri->e1->alpha0() == oldTri->e1->alpha1()){
-							curTri->e1->connectTo2(oldTri->e1);
-							oldTri->e1->connectTo2(curTri->e1);
+					if( (curTri.e1)->m_vertex == (oldTri.e1)->m_vertex){
+						if( (curTri.e1)->alpha0() == (oldTri.e1)->alpha1()){
+							curTri.e1->connectTo2(oldTri.e1);
+							oldTri.e1->connectTo2(curTri.e1);
 						}
 					}
-					if(curTri->e2->m_vertex == oldTri->e2->m_vertex){
-						if(curTri->e2->alpha0() == oldTri->e2->alpha1()){
-							curTri->e2->connectTo2(oldTri->e2);
-							oldTri->e2->connectTo2(curTri->e2);
+					
+					if( (curTri.e2)->m_vertex == (oldTri.e2)->m_vertex){
+						if(curTri.e2->alpha0() == oldTri.e2->alpha1()){
+							curTri.e2->connectTo2(oldTri.e2);
+							oldTri.e2->connectTo2(curTri.e2);
 						}
 					}
-					if(curTri->e3->m_vertex == oldTri->e3->m_vertex){
-						if(curTri->e3->alpha0() == oldTri->e3->alpha1()){
-							curTri->e3->connectTo2(oldTri->e3);
-							oldTri->e3->connectTo2(curTri->e3);
+					
+					if(curTri.e3->m_vertex == oldTri.e3->m_vertex){
+						if(curTri.e3->alpha0() == oldTri.e3->alpha1()){
+							curTri.e3->connectTo2(oldTri.e3);
+							oldTri.e3->connectTo2(curTri.e3);
 						}
 					}
 				}
